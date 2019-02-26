75c6b63d027143898c11cbcc9f7f86e4397dae8e
ws-samb <35712982+ws-samb@users.noreply.github.com>
2019-02-25 18:26:54 -0600

    Update MEGATest.ino

--- MEGATest.ino
+++ MEGATest.ino
@@ -3,20 +3,18 @@
 QTRSensorsRC qtrrc((unsigned char[]) {23, 25, 27, 29, 31, 33, 35, 37, 39, 41, 43, 45, 47, 49, 51, 53}, 16);
 
 unsigned int sensorValues[16];  
-
-int prevAverage[16];
-
 int average[16];
-
 double offFromAverage[16];
+double offFromAverageBy[16];
 
 int displayLoop = 0;
 
+byte lineLocation[1]; 
+
 void setup() {
   // put your setup code here, to run once:
+  lineLocation[0] = 1;
   Serial.begin(9600);
-  pinMode(3, OUTPUT);
-  pinMode(2, OUTPUT);
   Serial.print("test\n");
   qtrrc.emittersOn();
   qtrrc.calibrate();
@@ -28,31 +26,34 @@ void setup() {
 
 void loop() {
   qtrrc.read(sensorValues);
-  for(int i = 0; i < 16; i++) {
-    average[i] = (average[i] + sensorValues[i]) / 2;
-    offFromAverage[i] = (double)sensorValues[i] / average[i];
+  lineLocation[0] = 0; 
+  
+  if(displayLoop < 16) {
+     for(int i = 0; i < 16; i++) {
+      average[i] = (average[i] + sensorValues[i]) / 2;  
+    }
+    displayLoop++;  
   }
-  
-  
-  
-  
-  
-  for(int i = 0; i < 16; i++) {
-    Serial.print("\n");    
-    Serial.print("Sensor ");
-    Serial.print(i);
-    Serial.print(": ");
-    Serial.print(sensorValues[i]);
-    Serial.print("\n");    
-    Serial.print("Average ");
-    Serial.print(i);
-    Serial.print(": ");
-    Serial.print(offFromAverage[i]);
+  if(displayLoop == 16) {
+    Serial.print("Average Set!");
+    Serial.print("\n");
+    displayLoop++;  
+  }
 
-    
+  for(int i = 0; i < 16; i++) {
+    offFromAverage[i] = (double)sensorValues[i] / average[i];
+    offFromAverageBy[i] = abs(offFromAverage[i] - 1.0); 
+    if(offFromAverage[i] > offFromAverage[lineLocation[0]] ) {
+      lineLocation[0] = i; 
+    } 
   }
+
+  //Serial.write(lineLocation);
+  Serial.write(lineLocation[0]);
+  //Serial.print(lineLocation[0]);
+  //Serial.print("\n"  );
+  //Serial.write("Hello!");
   
-  
-  delay(500);
+  delay(500); 
   
 }
